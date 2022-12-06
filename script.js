912fdabec36b633b73134a5d249c44c0d6625b7a
bartammming <trab14@gmx.de>
2022-12-06 22:49:19 +0100

    Function hinzugefügt

---
+++ script.js
@@ -0,0 +1,47 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
+        "answer_1": "Text Fett",
+        "answer_2": "Container",
+        "answer_3": "Ein Link",
+        "answer_4": "Kursiv",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie heißt die ehemalige Forschungsabteilung der Telefongesellschaften AT&T?",
+        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+        "answer_2": "&lt;iframe&gt;",
+        "answer_3": "&lt;frame&gt;",
+        "answer_4": "&lt;frameset&gt;",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist die Vigenére-Chiffre?",
+        "answer_1": "monographische polyalphabetisches Substitutionsverfahren",
+        "answer_2": "Ein Schrank aus dem 16 Jahrhundert",
+        "answer_3": "Eine Plakette für die Französische Autobahn",
+        "answer_4": "Eine kryptographische Sicherheit",
+        "right_answer": 14
+    },
+    {
+        "question": "Was war die Strafe für Sisyphus, auch als Sisyphusaufgabe bekannt?",
+        "answer_1": "Die Verbannung vom Olymp",
+        "answer_2": "Ein Stein Immer wieder einen Berg hoch rollen",
+        "answer_3": "Erblindung",
+        "answer_4": "Nie wieder Minecraft zuspielen",
+        "right_answer": 2
+    },
+
+];
+
+function init(){
+    
+}
